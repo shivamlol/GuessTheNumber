d150da1b75c4e8562e02e07e38ed9beaefe8c07a
shivamlol <shivamchaurasia2912@gmail.com>
2022-08-16 20:23:06 +0530

    Added new Files/code snippets

--- app/src/main/java/com/example/guessthenumber/MainActivity.java
+++ app/src/main/java/com/example/guessthenumber/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.guessthenumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SecActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(MainActivity.this,ThirdActivity.class);
+                startActivity(intent2);
+            }
+        });
+
     }
+
+
 }
